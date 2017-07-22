ad63328954013c3d831e4f884dc7961fbe7d0474
chunkunwang <amos@cs.unc.edu>
2017-07-22 11:20:22 -0400

    fix some bugs and add export cert from ms

--- DropBox/6.initial.sh
+++ DropBox/6.initial.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-ADMIN_CERTS="/home/amos/syndicate-docker/release/ms/admin.pem"
 ADMIN_EMAIL="amos@cs.unc.edu"
+ADMIN_CERTS="/home/amos/syndicate-docker/release/ms/admin.pem"
 ADMIN_PEM="./admin.pem"
+
+#Copy out admin certs from MS's container
+cd /home/amos/syndicate-docker/release/ms
+make copy_cert
+cd /home/amos/Syndicate-script/DropBox
 
 # Copy in admin certs
 docker cp $ADMIN_CERTS ms:/home/syndicate/
@@ -35,10 +40,22 @@ docker exec -ti ms /bin/bash -c "$COMMAND"
 COMMAND="syndicate create_gateway email=$ADMIN_EMAIL volume=test-volume name=RG01 private_key=auto type=RG caps=ALL host=rg"
 docker exec -ti ms /bin/bash -c "$COMMAND"
 
+#Export cert from ms
+COMMAND="syndicate export_gateway RG01 ."
+docker exec -ti ms /bin/bash -c "$COMMAND"
+
+docker cp ms:/home/syndicate/RG01 .
+docker cp RG01 rg:/home/syndicate/
+
+COMMAND="syndicate export_gateway test-volume ."
+docker exec -ti ms /bin/bash -c "$COMMAND"
+
+docker cp ms:/home/syndicate/test-volume .
+docker cp test-volume rg:/home/syndicate/
 
 #Setup disk RG driver
 #Src: https://github.com/syndicate-storage/syndicate-core/tree/master/python/syndicate/rg/drivers/disk
-docker exec -ti rg /bin/bash "mkdir download"
+docker exec -ti rg /bin/bash -c "mkdir download"
 docker cp config rg:/home/syndicate/download
 docker cp secrets rg:/home/syndicate/download
 docker cp driver rg:/home/syndicate/download
