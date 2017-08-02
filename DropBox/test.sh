13d803d96c79c5b67334f1147b1256ed0ab7b5c6
chunkunwang <amos@cs.unc.edu>
2017-08-02 15:10:18 -0400

    delete the file moving to the dir

--- DropBox/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-IMAGE_NAME="syndicate-docker-gateways"
-NETWORK_NAME="syndicate-docker"
-
-# set syndicate
-docker run --net $NETWORK_NAME -ti --cap-add SYS_ADMIN --device /dev/fuse --privileged --name ag $IMAGE_NAME
-
-
